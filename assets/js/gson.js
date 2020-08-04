859cc5c2c9e97a534b3952f6d9d67f3372e1519e
Matthias Singer <matze.singer@gmail.com>
2020-08-04 08:02:30 +0200

    update

--- assets/js/gson.js
+++ assets/js/gson.js
@@ -2,12 +2,32 @@ import fetch from 'axios';
 import mock from './mock.json';
 
 export async function getData(sheetsURL) {
-    const gson = "https://gson.fayazara.now.sh/?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vT6-XSxmzjwS6tA1rbT8YVp0hUG7Gnat4hT_H1_ti0hNTdnCE7J9R3YUI5peuGqLPc6dDqhFUH13dfj/pub?output=csv"
+  const gson ="https://gson.fayazara.now.sh/?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vQvPQm7xh1xESblJ4Np_NK7pQJZcL1o5bVUtezieIjLwiRsEvK3-7VI9iS93tr8b3UqqVA9ScR5qV6x/pub?output=csv" 
 
   const grab = await fetch(gson);
 
-  return grab.data;
+  //return grab.data;
+  const data = grab.data;
+  return transformData(data);
   // return mock;
+}
+
+function transformData(data) {
+    var res = [
+        {key: 'ally-total'},
+        {key: 'ally-current'},
+        {key: 'horde-total'},
+        {key: 'horde-current'}
+    ];
+
+    for (var row of data) {
+        res[0][row.item1] = row['ally-total'];
+        res[1][row.item1] = row['ally-current'];
+        res[2][row.item2] = row['horde-total'];
+        res[3][row.item2] = row['horde-current'];
+    }
+
+    return res;
 }
 
 // old gson link
